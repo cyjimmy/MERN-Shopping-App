af2037d3eb5f7ef2a25b82fa4acf9e6bac0f1268
Chun Yip Wong <cy.jimmy.wong@gmail.com>
2022-01-05 08:07:52 -0800

    reversed heroku changes

--- server.js
+++ server.js
@@ -6,13 +6,6 @@ const path = require("path");
 
 dotenv.config();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 const PORT = process.env.PORT || 8080;
 const MONGO_URI = process.env.DATABASE_URL;
 
