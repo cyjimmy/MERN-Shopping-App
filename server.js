cb294385f6206a39353d77e6e346d51fea12deb1
Chun Yip Wong <cy.jimmy.wong@gmail.com>
2022-01-05 07:30:45 -0800

    setup heroku

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const productRoutes = require("./routes/products");
+const path = require("path");
 
 dotenv.config();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 8080;
 const MONGO_URI = process.env.MONGO_URI;
@@ -12,7 +20,7 @@ mongoose.connect(MONGO_URI).then(() => {
   const app = express();
   const cart = [];
 
-  app.use(express.json())
+  app.use(express.json());
 
   app.use("/products", productRoutes);
 
